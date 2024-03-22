cd9f5d32d05791e78db4d1e2c09a045341c2f8d9
tienratdeptrai <117457885+Tiendepchai@users.noreply.github.com>
2024-03-22 17:44:31 +0700

    for new ubuntu

---
+++ newUbuntu.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade
+sudo apt purge aisleriot gnome-sudoku gnome-mahjongg cheese-common gnome-mines totem 
+sudo apt autoremove && sudo apt clean
+sudo snap install vlc g++ gcc
+sudo snap install --classic code
+sudo apt install google-chrome-stable
+sudo apt install gnome-tweaks
+sudo apt install chrome-gnome-shell
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 
