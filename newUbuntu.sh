56124126cb29041d07090b9171e83d8e3571ce14
Nguyen Kong Tien <117457885+Tiendepchai@users.noreply.github.com>
2024-05-06 21:18:47 +0700

    Update newUbuntu.sh

--- newUbuntu.sh
+++ newUbuntu.sh
@@ -1,9 +1,9 @@
 sudo apt update && sudo apt upgrade
 sudo apt purge aisleriot gnome-sudoku gnome-mahjongg cheese-common gnome-mines totem 
-sudo apt autoremove && sudo apt clean
 sudo snap install vlc
 sudo apt install g++ gcc
 sudo snap install --classic code
 sudo apt install google-chrome-stable
 sudo apt install gnome-tweaks
 sudo apt install chrome-gnome-shell
+sudo apt autoremove && sudo apt clean
